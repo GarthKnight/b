4bbcc3916a0da406b538b1e14ba942262c277542
Yana Perhun <janaperhun@gmail.com>
2017-04-13 14:53:34 +0300

    fix

--- app/src/main/java/com/appb/app/appb/ui/dialogs/AnswersForPostDialog.java
+++ app/src/main/java/com/appb/app/appb/ui/dialogs/AnswersForPostDialog.java
@@ -2,17 +2,24 @@ package com.appb.app.appb.ui.dialogs;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 
 import com.appb.app.appb.R;
 import com.appb.app.appb.custom.TextViewWithClickableSpan;
 import com.appb.app.appb.data.Post;
+import com.appb.app.appb.ui.activities.PicViewerActivity;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.appb.app.appb.ui.activities.PicViewerActivity.FILES;
+import static com.appb.app.appb.ui.activities.PicViewerActivity.POS;
 
 /**
  * Created by 1 on 31.03.2017.
@@ -42,6 +49,7 @@ public class AnswersForPostDialog extends Dialog {
             getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         }
         setContentView(R.layout.dialog_answers);
+        ButterKnife.bind(this);
         init();
     }
 
@@ -69,11 +77,23 @@ public class AnswersForPostDialog extends Dialog {
                 }
 
                 if (tmp != -1) {
-                    AnswerDialog answerDialog = new AnswerDialog(getContext(), posts, tmp);
+                    AnswerDialog answerDialog = new AnswerDialog(getContext(), posts, tmp){
+                        @Override
+                        public void onItemClick(View v, int position, int pos) {
+                            startPicViewerActivity(position, pos);
+                        }
+                    };
                     answerDialog.show();
                 }
             }
         });
+    }
+
+    private void startPicViewerActivity(int position, int pos) {
+        Intent intent = new Intent(getContext(), PicViewerActivity.class);
+        intent.putExtra(FILES, posts.get(position).getFiles());
+        intent.putExtra(POS, pos);
+        getContext().startActivity(intent);
     }
 
 
