2f819499c3237ad557e0a84af9fcef3d3a28371b
Garth_Knight <st.logvinov@gmail.com>
2018-05-02 19:00:57 +0300

    added vk sharing

--- app/src/main/java/com/appb/app/appb/ui/fragments/PostListFragment.java
+++ app/src/main/java/com/appb/app/appb/ui/fragments/PostListFragment.java
@@ -1,5 +1,6 @@
 package com.appb.app.appb.ui.fragments;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -8,6 +9,8 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +18,7 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
 import com.appb.app.appb.R;
+import com.appb.app.appb.api.API;
 import com.appb.app.appb.data.DvachMediaFile;
 import com.appb.app.appb.data.Post;
 import com.appb.app.appb.mvp.presenters.PostListPresenter;
@@ -26,6 +30,10 @@ import com.appb.app.appb.ui.dialogs.AnswerDialog;
 import com.appb.app.appb.utils.ViewUtils;
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.bumptech.glide.Glide;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
 import com.vk.sdk.api.VKError;
 import com.vk.sdk.api.photo.VKImageParameters;
 import com.vk.sdk.api.photo.VKUploadImage;
@@ -55,6 +63,7 @@ public class PostListFragment extends BaseFragment implements PostListView {
     private static final String POSTS = "posts";
     private static final String THREAD_NUMBER = "num";
     private static final String BOARD = "board";
+    private static final String TAG = "PostListFragment";
 
     private PostsAdapter postsAdapter;
     private ArrayList<Post> posts = new ArrayList<>();
@@ -126,29 +135,35 @@ public class PostListFragment extends BaseFragment implements PostListView {
             @Override
             public void onShareClick(Post post) {
                 super.onShareClick(post);
-                downloadBitmaps(post)
-                        .subscribeOn(Schedulers.newThread())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(new Subscriber<ArrayList<Bitmap>>() {
-                            @Override
-                            public void onCompleted() {
+                if (VKSdk.isLoggedIn()) {
+                    downloadBitmaps(post)
+                            .subscribeOn(Schedulers.newThread())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(new Subscriber<ArrayList<Bitmap>>() {
+                                @Override
+                                public void onCompleted() {
 
-                            }
-
-                            @Override
-                            public void onError(Throwable e) {
-
-                            }
-
-                            @Override
-                            public void onNext(ArrayList<Bitmap> bitmaps) {
-                                VKUploadImage[] vkUploadImages = new VKUploadImage[bitmaps.size()];
-                                for (int i = 0; i < bitmaps.size(); i++) {
-                                    vkUploadImages[i] = new VKUploadImage(bitmaps.get(i), VKImageParameters.pngImage());
                                 }
-                                showDialog(vkUploadImages, post);
-                            }
-                        });
+
+                                @Override
+                                public void onError(Throwable e) {
+
+                                }
+
+                                @Override
+                                public void onNext(ArrayList<Bitmap> bitmaps) {
+                                    VKUploadImage[] vkUploadImages = new VKUploadImage[bitmaps.size()];
+                                    for (int i = 0; i < bitmaps.size(); i++) {
+                                        vkUploadImages[i] = new VKUploadImage(bitmaps.get(i), VKImageParameters.pngImage());
+                                    }
+                                    showDialog(vkUploadImages, post);
+                                }
+                            });
+                } else {
+                    VKSdk.login(getActivity(), VKScope.PHOTOS, VKScope.WALL);
+                }
+
+
             }
         };
 
@@ -160,7 +175,8 @@ public class PostListFragment extends BaseFragment implements PostListView {
             ArrayList<Bitmap> bitmaps = new ArrayList<>();
             for (int i = 0; i < post.getDvachMediaFiles().size(); i++) {
                 try {
-                    Bitmap bitmap = Glide.with(getContext()).load(post.getDvachMediaFiles().get(i).getPath()).asBitmap().into(-1, -1).get();
+                    String fileUrl = API.URL + post.getDvachMediaFiles().get(i).getPath();
+                    Bitmap bitmap = Glide.with(getContext()).load(fileUrl).asBitmap().into(-1, -1).get();
                     bitmaps.add(bitmap);
                 } catch (InterruptedException | ExecutionException e) {
                     e.printStackTrace();
@@ -172,24 +188,26 @@ public class PostListFragment extends BaseFragment implements PostListView {
 
     private void showDialog(VKUploadImage[] vkUploadImages, Post post) {
         VKShareDialogBuilder builder = new VKShareDialogBuilder();
-        builder.setText(post.getComment());
+        builder.setText(Html.fromHtml(post.getComment()));
         if (vkUploadImages.length > 0) {
             builder.setAttachmentImages(vkUploadImages);
         }
+        builder.setAttachmentLink("Сосач",
+                API.URL);
         builder.setShareDialogListener(new VKShareDialogBuilder.VKShareDialogListener() {
             @Override
             public void onVkShareComplete(int postId) {
-
+                Log.d(TAG, "onVkShareComplete: " + String.valueOf(postId));
             }
 
             @Override
             public void onVkShareCancel() {
-
+                Log.d(TAG, "onVkShareCancel");
             }
 
             @Override
             public void onVkShareError(VKError error) {
-
+                Log.d(TAG, "onVkShareError: " + error.errorMessage);
             }
         });
         builder.show(getChildFragmentManager(), "VK_SHARE_DIALOG");
@@ -200,6 +218,22 @@ public class PostListFragment extends BaseFragment implements PostListView {
         intent.putExtra(FILES, dvachMediaFiles);
         intent.putExtra(POS, pos);
         startActivity(intent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // Пользователь успешно авторизовался
+            }
+
+            @Override
+            public void onError(VKError error) {
+                // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+            }
+        }))
+            super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
